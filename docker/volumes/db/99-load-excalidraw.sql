18745df0eacf3ec22af3f92d3aa61d2068b0d220
Yahir Navarro <Yahir_navarro@outlook.es>
2026-06-07 12:04:23 -0700

    feat: update Docker configuration and add initialization scripts for Supabase

---
+++ docker/volumes/db/99-load-excalidraw.sql
@@ -0,0 +1,9 @@
+-- Wrapper: loads Excalidraw custom init scripts from /excalidraw-init/
+\i /excalidraw-init/seed.sql
+\i /excalidraw-init/init-scripts/98-webhooks.sql
+\i /excalidraw-init/init-scripts/99-set-passwords.sql
+\i /excalidraw-init/init-scripts/99-jwt.sql
+\i /excalidraw-init/migrations/97-_supabase.sql
+\i /excalidraw-init/migrations/98-pooler.sql
+\i /excalidraw-init/migrations/99-logs.sql
+\i /excalidraw-init/migrations/9999-app.sql
